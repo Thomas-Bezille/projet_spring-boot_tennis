6487d06bcecfec64cd0085b7ea457003accab7d8
thomas <bezille.thomasb@gmail.com>
2024-07-26 13:42:44 +0200

    Mise en place des parties Controller et Service

--- src/main/java/com/dyma/tennis/rest/HealthCheckController.java
+++ src/main/java/com/dyma/tennis/rest/HealthCheckController.java
@@ -1,13 +1,19 @@
 package com.dyma.tennis.rest;
 
+import com.dyma.tennis.HealthCheck;
+import com.dyma.tennis.service.HealthCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
+    @Autowired
+    private HealthCheckService healthCheckService;
+
     @GetMapping("/healthcheck")
     public HealthCheck healthCheck() {
-        return new HealthCheck(ApplicationStatus.OK, "Welcome to Dyma Tennis !");
+        return healthCheckService.healthCheck();
     }
 }
