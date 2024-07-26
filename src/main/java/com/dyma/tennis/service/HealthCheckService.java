02f59c931fb7e54df086f15cb99396537c6a3ff9
thomas <bezille.thomasb@gmail.com>
2024-07-26 18:29:50 +0200

    Create HealthCheckRepository

--- src/main/java/com/dyma/tennis/service/HealthCheckService.java
+++ src/main/java/com/dyma/tennis/service/HealthCheckService.java
@@ -2,12 +2,23 @@ package com.dyma.tennis.service;
 
 import com.dyma.tennis.ApplicationStatus;
 import com.dyma.tennis.HealthCheck;
+import com.dyma.tennis.repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
 
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
     public HealthCheck healthCheck() {
-        return new HealthCheck(ApplicationStatus.OK, "Welcome to Dyma Tennis !");
+        Long activeSession = healthCheckRepository.countApplicationConnections();
+
+        if (activeSession > 0) {
+            return new HealthCheck(ApplicationStatus.OK, "Dyma Tennis");
+        } else {
+            return new HealthCheck(ApplicationStatus.KO, "Dyma Tennis is not fully functional, please check your configuration !");
+        }
     }
 }
