4e3c649786921075f8306913e050fc09416a94bd
thomas <bezille.thomasb@gmail.com>
2024-07-30 11:34:53 +0200

    Find all players

--- src/main/java/com/dyma/tennis/web/PlayerController.java
+++ src/main/java/com/dyma/tennis/web/PlayerController.java
@@ -2,6 +2,7 @@ package com.dyma.tennis.web;
 
 import com.dyma.tennis.Player;
 import com.dyma.tennis.PlayerList;
+import com.dyma.tennis.service.PlayerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -20,6 +22,9 @@ import java.util.List;
 @RequestMapping("/players")
 public class PlayerController {
 
+    @Autowired
+    private PlayerService playerService;
+
     @Operation(summary = "Finds players", description = "Find players")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Players list",
@@ -28,7 +33,7 @@ public class PlayerController {
     })
     @GetMapping
     public List<Player> list() {
-        return PlayerList.ALL;
+        return playerService.getAllPlayers();
     }
 
 
